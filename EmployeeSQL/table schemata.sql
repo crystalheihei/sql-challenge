e54b64345740d2038b3215962e0654d69d97f1e0
Crystal Wang <118711472+crystalheihei@users.noreply.github.com>
2023-02-12 17:43:19 -0800

    Update table schemata.sql

--- EmployeeSQL/table schemata.sql
+++ EmployeeSQL/table schemata.sql
@@ -1,34 +1,65 @@
-departments
--
-dept_no PK VARCHAR
-dept_name VARCHAR
+CREATE TABLE departments(
+	dept_no VARCHAR PRIMARY KEY,
+	dept_name VARCHAR
+);
 
-dept_emp
--
-emp_no PK INT FK >- employees.emp_no
-dept_no PK VARCHAR FK >- departments.dept_no
+SELECT *
+FROM departments
 
-dept_manager
--
-dept_no PK VARCHAR FK >- departments.dept_no
-emp_no PK INT FK >- employees.emp_no
 
-employees
--
-emp_no PK INT
-emp_title_id VARCHAR FK >- titles.title_id
-birth_date DATE
-first_name VARCHAR
-last_name VARCHAR
-sex VARCHAR
-hire_date DATE
+CREATE TABLE titles(
+	title_id VARCHAR PRIMARY KEY,
+	title VARCHAR NOT NULL
+);
 
-salaries
--
-emp_no INT FK >- employees.emp_no
-salary INT
+SELECT *
+FROM titles
 
-titles
--
-title_id PK VARCHAR
-title VARCHAR
+
+CREATE TABLE employees(
+	emp_no INT PRIMARY KEY,
+	emp_title_id VARCHAR NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	sex VARCHAR NOT NULL,
+	hire_date DATE NOT NULL,
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+SELECT *
+FROM employees
+
+
+CREATE TABLE dept_emp(
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	PRIMARY KEY (emp_no, dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+SELECT *
+FROM dept_emp
+
+
+CREATE TABLE dept_manager(
+	dept_no VARCHAR NOT NULL,
+	emp_no INT NOT NULL,
+	PRIMARY KEY (dept_no, emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+SELECT *
+FROM dept_manager
+
+
+CREATE TABLE salaries(
+	emp_no INT PRIMARY KEY,
+	salary INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+SELECT *
+FROM salaries
